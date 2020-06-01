bbb56d99320b20e82a7650330c145872af9f3d2b
George <327052186@qq.com>
2020-06-01 14:16:03 +0800

    娱乐项目截稿

--- src/main/java/com/cognizant/ams/scheduler/KaiJiangPLCScheduler.java
+++ src/main/java/com/cognizant/ams/scheduler/KaiJiangPLCScheduler.java
@@ -1,8 +1,6 @@
 package com.cognizant.ams.scheduler;
 
 import java.text.SimpleDateFormat;
-import java.util.HashSet;
-import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.cognizant.ams.bean.SmFun;
@@ -18,19 +16,15 @@ public class KaiJiangPLCScheduler {
 
 	public void run() {
 		// 产生随机开奖号码
-		Set<Integer> set9 = new HashSet<Integer>();
+		//Set<Integer> set9 = new HashSet<Integer>();
 		String daxiao="大";
-		int arr9[] = new int[10];
-		while (set9.size() < 3) {
-			int a = (int) Math.ceil(Math.random() * 9);
-			set9.add(a);
+		int arr9[] = new int[3];
+		
+		for (int i=0;i<3;i++) {
+			arr9[i] =(int) Math.ceil(Math.random() * 10);
 		}
 		int a = (int) Math.ceil(Math.random() * 2);
-		int m = 0;
-		for (int i : set9) {
-			arr9[m] = i;
-			m++;
-		}
+	
 		SmFun smFun = new SmFun();
 		for (int i = 0; i < arr9.length; i++) {
 			if (i == 0) {
@@ -49,16 +43,18 @@ public class KaiJiangPLCScheduler {
 		smFun.setFval9(daxiao);
 		smFun.setType("PLC");
 		smFun.setFval8("sys");
+		//System.out.println(arr9[0]+","+arr9[1]+","+arr9[2]);
 		funService.kaiJiang(smFun);
 		System.out.println("PLC开奖执行成功");
 		System.out.println("开始自动兑奖");
+		
 		//先获取本次开奖结果。 2.获取本期所有投注  3.判断是否中奖，4.按奖项新增积分
 		funService.duijiang(smFun);
 		System.out.println("自动兑奖完成");
 	}
 
 	public static void main(String[] args) throws Exception {
-		//new KaiJiangPLCScheduler().run();
-		System.out.println(Math.ceil(Math.random() * 9));
+		new KaiJiangPLCScheduler().run();
+		//System.out.println(Math.ceil(Math.random() * 9));
 	}
 }
